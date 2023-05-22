03733c6c60bc8d62c099fcdcc8a7f154a8e15bea
tratchapong <tratchapong@gmail.com>
2023-05-22 17:42:29 +0700

    register - login - getMe

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const authController = require('../controllers/authController')
+const authenticate = require('../middlewares/authenticate')
 const router = express.Router()
 
 router.post('/register', authController.register)
 router.post('/login', authController.login)
+router.get('/getMe', authenticate, authController.getMe)
 
 module.exports = router
